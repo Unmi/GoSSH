db5181581e6d4ba0ce42490c9adf06f6ba71982d
Unmi Qiu <unmi.qiu@morningstar.com>
2014-02-16 00:44:57 -0600

    add source folder

---
+++ src/main/java/cc/unmi/Main.java
@@ -0,0 +1,168 @@
+package cc.unmi;
+
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.UIKeyboardInteractive;
+import com.jcraft.jsch.UserInfo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Options options = new Options();
+		options.addOption(Option.builder("f").longOpt("file").hasArg().required().desc("IP Address list file").build());
+		options.addOption(Option.builder("u").longOpt("username").hasArg().required().desc("Username").build());
+		options.addOption(Option.builder("p").longOpt("password").hasArg().desc("Password").build());
+		options.addOption(Option.builder("c").longOpt("command").hasArg().required().desc("Command").build());
+		CommandLineParser parser = new DefaultParser();
+		
+		CommandLine cmd = null;
+		try {
+			cmd = parser.parse( options, args);
+		} catch (ParseException e1) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp( "gossh", options );
+			System.exit(-1);
+		}
+		
+		String file = cmd.getOptionValue("f");
+		String username = cmd.getOptionValue("u");
+		String password = cmd.getOptionValue("p");
+		String command = cmd.getOptionValue("c");
+
+		if(password == null || password.trim().isEmpty()){
+
+			if(System.console() != null){
+				char[] pwd = System.console().readPassword("Password: ");
+				password = new String(pwd);
+			}else{
+				System.out.print("Password: ");
+				Scanner input = new Scanner(System.in);
+				password = input.next();
+			}
+		}
+		
+		List<String> hosts = loadIPAddressFromFile(file);
+
+		try {
+			JSch jsch = new JSch();
+			java.util.Properties config = new java.util.Properties(); 
+			config.put("StrictHostKeyChecking", "no");
+			
+			for (String host : hosts) {
+				Session session = jsch.getSession(username, host, 22);
+
+				// username and password will be given via UserInfo interface.
+//				UserInfo ui = new MyUserInfo();
+//				session.setUserInfo(ui);
+				session.setPassword(password);
+				
+				session.setConfig(config);
+				
+				session.connect();
+
+				Channel channel = session.openChannel("exec");
+				((ChannelExec) channel).setCommand(command);
+
+				// X Forwarding
+				// channel.setXForwarding(true);
+
+				// channel.setInputStream(System.in);
+				channel.setInputStream(null);
+
+				// channel.setOutputStream(System.out);
+
+				// FileOutputStream fos=new FileOutputStream("/tmp/stderr");
+				// ((ChannelExec)channel).setErrStream(fos);
+				((ChannelExec) channel).setErrStream(System.err);
+
+				InputStream in = channel.getInputStream();
+
+				channel.connect();
+
+				byte[] tmp = new byte[1024];
+				while (true) {
+					while (in.available() > 0) {
+						int i = in.read(tmp, 0, 1024);
+						if (i < 0)
+							break;
+						System.out.print(new String(tmp, 0, i));
+					}
+					if (channel.isClosed()) {
+						System.out.println("exit-status: " + channel.getExitStatus());
+						break;
+					}
+					try {
+						Thread.sleep(1000);
+					} catch (Exception ee) {
+					}
+				}
+				channel.disconnect();
+				session.disconnect();
+			}
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public static List<String> loadIPAddressFromFile(String ipFile){
+		File file = new File(ipFile);
+		List<String> ips = new ArrayList<String>();
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			String line = null;
+			while((line=br.readLine()) != null){
+				if(!line.trim().isEmpty()){
+					ips.add(line.trim());
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(ipFile + " does not exist");
+			System.exit(-1);
+		} catch (IOException e){
+			System.out.println("read " + ipFile + " error.");
+			System.exit(-2);
+		} finally{
+			if(br != null){
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ips;
+	}
+}
